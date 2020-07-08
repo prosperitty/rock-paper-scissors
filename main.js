0bbd916df409a1dca87d76512ca73cf043fced09
Alex Marmolejo <alex97marmolejo@gmail.com>
2020-07-07 21:42:20 -0400

    Added and styled scoreboard.made a function that randomly picks the computer's play. Added functionality that detects the user's play. Created a function that updates some of the text to reflect both player's choices.

--- main.js
+++ main.js
@@ -1,45 +1,78 @@
-let playerSelection = ['rock','paper','scissor'];
-const computerSelection = "scissor";
 const message = document.querySelector('#message');
-const selection = document.querySelectorAll('.select');
-const choiceMessage = document.querySelector('#choice');
-const choice = [];
+const selections = document.querySelectorAll('.selection');
+const playerText = document.querySelector('#playerChoice');
+const computerText = document.querySelector('#computerChoice')
+const rockPaperScissor = ['rock','paper','scissor'];
+let playerChoice = undefined
+let computerChoice = undefined;
+let computerTurn = false
+let randomNum = undefined;
     
 
+playRound();
 
-
-function playerPlay() {
-    for (const i in selection) {
-        selection[i].addEventListener("click", function() {
-            if (selection[i] === selection[0]) {
-                choice.splice(0, 1, playerSelection[0])
-                return choice.textContent = playerSelection[0];
+function playRound() {
+    for (const i in selections) {
+        selections[i].addEventListener("click", function() {
+            if (selections[i]) {
+                playerChoice = rockPaperScissor[i];
+            } else {
+                return console.error(failure);
             }
-            else if (selection[i] === selection[1]){
-                choice.splice(0, 1, playerSelection[1])
-                return choice.textContent = playerSelection[1];
-            }
-            else if (selection[i] === selection[2]){
-                return choice.textContent = playerSelection[2];
-            };
+            computerTurn = true;
+            computerPlay();
+            decisions(playerChoice, computerChoice);
+            winner();
+            return computerTurn = false;
         });
     };
 };
 
-// function computerPlay() {
-//     return computerSelection;
-// }
+function randomMove() {
+    return randomNum = Math.floor(Math.random() * Math.floor(3))
+}
 
-function playRound (playerSelection, computerSelection) {
+function computerPlay() {
+    if (computerTurn === true) {
+        randomMove();
+        return computerChoice = rockPaperScissor[randomNum];
+    };
+};
+
+function decisions(playerSelection, computerSelection) {
     console.log(`Player chose ${playerSelection}, Computer chose ${computerSelection}`);
-    message.innerHTML = `Player chose ${playerSelection}, Computer chose ${computerSelection}`
+    message.textContent = `Player chose ${playerSelection}, Computer chose ${computerSelection}`;
+    playerText.textContent = playerSelection;
+    computerText.textContent = computerSelection;
 }
 
 function winner() {
-
+    if (playerChoice === computerChoice){
+        console.log('tie!');
+    }; 
+    if (playerChoice !== computerChoice){
+        if (playerChoice === "paper" && computerChoice === "rock") {
+            console.log('paper wins');
+        } else if (computerChoice === "scissor") {
+            console.log('scissor wins!')
+        }
+            // if (computerChoice === "rock") {
+            //     console.log("paper wins");
+            // } else {
+            //     if (computerChoice === "scissors") {
+            //         console.log("scissors wins");
+            //     }
+            // }
+        
+        // if (playerChoice === "scissors") {
+        //     if (computerChoice === "rock") {
+        //         console.log("rock wins");
+        //     } else {
+        //         if (computerChoice === "paper") {
+        //             console.log("scissors wins") ;
+        //         }
+        //     }
+        // }
+    }
 }
 
-playerPlay();
-
-
-// runGame();
